4332efdde94e0141001ec0bb24982c761b1a5373
Philipp Göldner <philipp.goeldner@gmail.com>
2021-03-27 15:49:46 +0100

    cleanup autoencoder code

--- autoencoder/eval.py
+++ autoencoder/eval.py
@@ -1,38 +1,10 @@
-from torch.utils.data import TensorDataset, DataLoader, Dataset
-from torchvision import transforms
+from torch.utils.data import TensorDataset, DataLoader
 
 from model import AutoEncoder
 import torch.nn as nn
 import torch
 import numpy as np
 from tqdm import tqdm
-from torch.utils.tensorboard import SummaryWriter
-
-
-import random
-from typing import Sequence
-
-class MyRotateTransform:
-    def __init__(self, angles: Sequence[int]):
-        self.angles = angles
-
-    def __call__(self, x):
-        angle = random.choice(self.angles)
-        return transforms.functional.rotate(x, angle)
-
-class CustomTensorDataset(Dataset):
-    def __init__(self, *tensors, transform=None):
-        assert all(tensors[0].size(0) == tensor.size(0) for tensor in tensors), "Size mismatch between tensors"
-        self.tensors = tensors
-        self.transform = transform
-
-    def __getitem__(self, index):
-        if self.transform:
-            tuple(self.transform(tensor[index]) for tensor in self.tensors)
-        return tuple(tensor[index] for tensor in self.tensors)
-
-    def __len__(self):
-        return self.tensors[0].size(0)
 
 
 model = AutoEncoder().cuda()
@@ -41,29 +13,28 @@ criterion = nn.L1Loss()
 model.load_state_dict(checkpoint)
 model.eval()
 
-with open("agent_code/rule_based_agent_auto_encode/states.npy", "rb") as f:
+with open("../agent_code/rule_based_agent_auto_encode/states.npy", "rb") as f:
     data = np.load(f, allow_pickle=False)
-data_t = torch.Tensor(data).unsqueeze(1)
+
+data = data[:2_500_000]
+data_t = torch.Tensor(data)
 data_t = data_t.cuda()
-
-#transformations = transforms.Compose([transforms.RandomHorizontalFlip(), transforms.RandomVerticalFlip(), MyRotateTransform((90,180,270))])
-
-
-#dataset = CustomTensorDataset(data_t, transform=transformations)
 dataset = TensorDataset(data_t)
-batch_size = 128
-dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
 
-eval_loss = []
-for data in tqdm(dataloader):
+train_len = int(0.7*len(dataset))
+valid_len = (len(dataset)-train_len)//2
+test_len = len(dataset) - train_len - valid_len
+
+train_dataset, valid_dataset, test_dataset = torch.utils.data.random_split(dataset, [train_len, valid_len, test_len], generator=torch.Generator().manual_seed(42))
+
+batch_size = 32
+dataloader_test = DataLoader(test_dataset, batch_size=batch_size, shuffle=True)
+
+test_loss = []
+for data in tqdm(dataloader_test):
     img = data[0]
     img = img.view(img.shape[0], -1)
     output = model(img)
-    img_nn = (img * 12) - 1
-    output_nn = ((output * 12) - 1).round()
-    loss = criterion(output_nn, img_nn)
-    #torch.sum(torch.all(img_nn == output_nn, 0))
-    eval_loss.append(loss.item())
-print(np.mean(eval_loss))
-
-#torch.save(model.state_dict(), './autoencoder.pth')
+    loss = criterion(output, img)
+    test_loss.append(loss.item())
+print(np.mean(test_loss))
--- autoencoder/main.py
+++ autoencoder/main.py
@@ -13,29 +13,44 @@ model = AutoEncoder().cuda()
 criterion = nn.MSELoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
 
-with open("agent_code/rule_based_agent_auto_encode/states_no_dup.npy", "rb") as f:
+with open("../agent_code/rule_based_agent_auto_encode/states.npy", "rb") as f:
     data = np.load(f, allow_pickle=False)
-data = data[:1_000_000]
-data_t = torch.Tensor(data)#.unsqueeze(1)
+data = data[:2_500_000]
+data_t = torch.Tensor(data)
 data_t = data_t.cuda()
-
 dataset = TensorDataset(data_t)
 
-batch_size = 32
-dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
+train_len = int(0.7*len(dataset))
+valid_len = (len(dataset)-train_len)//2
+test_len = len(dataset) - train_len - valid_len
 
-for epoch in range(100):
-    epoch_loss = []
-    for data in tqdm(dataloader):
+train_dataset, valid_dataset, test_dataset = torch.utils.data.random_split(dataset, [train_len, valid_len, test_len], generator=torch.Generator().manual_seed(42))
+batch_size = 32
+dataloader_train = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+dataloader_valid = DataLoader(valid_dataset, batch_size=batch_size, shuffle=True)
+
+for epoch in range(50):
+    train_loss = []
+    valid_loss = []
+    for data in tqdm(dataloader_train):
         img = data[0]
         img = img.view(img.shape[0], -1)
         output = model(img)
         loss = criterion(output, img)
-        epoch_loss.append(loss.item())
+        train_loss.append(loss.item())
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
-    print(f"epoch: {epoch}, Loss: {np.mean(epoch_loss)}")
-    writer.add_scalar("Loss", np.mean(epoch_loss), epoch)
+
+    for data in tqdm(dataloader_valid):
+        img = data[0]
+        img = img.view(img.shape[0], -1)
+        output = model(img)
+        loss = criterion(output, img)
+        valid_loss.append(loss.item())
+    print(f"epoch: {epoch}, Train Loss: {np.mean(train_loss)}")
+    print(f"epoch: {epoch}, Valid Loss: {np.mean(valid_loss)}")
+    writer.add_scalar("Train loss", np.mean(train_loss), epoch)
+    writer.add_scalar("Valid loss", np.mean(valid_loss), epoch)
 
 torch.save(model.state_dict(), './autoencoder.pth')
